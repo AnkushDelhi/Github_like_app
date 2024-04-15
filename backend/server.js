0dab85801dfd807fbf0f7dfd306ea82ff8671b3e
AnkushDelhi <huddaankush8@gmail.com>
2024-04-15 22:44:32 +0530

    package json updated

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import session from 'express-session';
 import passport from 'passport';
+import path from 'path';
 
 import "./passport/github.auth.js";
 import userRoutes from './routes/user.route.js';
@@ -13,7 +14,9 @@ import connectMongoDB from '../db/connectMongoDB.js';
 
 
 dotenv.config();
+const PORT = process.env.PORT  || 5000;
 const app=express();
+const __dirname = path.resolve();
 
 app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 // Initialize Passport!  Also use passport.session() middleware, to support
@@ -22,16 +25,18 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors());
 
-app.get("/",(req,res)=>{
-    res.send("Server Activate");
-})
 
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/explore",exploreRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/vite-project/dist")));
 
-app.listen(5000,()=>{
-    console.log("server started on http://localhost:5000");
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname, "frontend","vite-project","dist","index.html"));
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server start on the ${PORT}`);
     connectMongoDB();
 });
