2213ccc2d6bc17997a29ff137ab43cd6dfd95ba5
AnkushDelhi <huddaankush8@gmail.com>
2024-04-17 12:05:42 +0530

    5000 request per hour

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -5,12 +5,20 @@ export const getUserProfileAndRepos = async (req, res) => {
 	const { username } = req.params;
 	try {
 		
-		const userRes = await fetch(`https://api.github.com/users/${username}`, 
+		const userRes = await fetch(`https://api.github.com/users/${username}`, {
+			headers:{
+				authorization: `token ${process.env.VITE_GITHUB_API_KEY}`,
+			}
+		}
 	);
 
 		const userProfile = await userRes.json();
 
-		const repoRes = await fetch(userProfile.repos_url );
+		const repoRes = await fetch(userProfile.repos_url ,{
+			headers:{
+				authorization:`token ${process.env.VITE_GITHUB_API_KEY}`,
+			}
+		})
 		const repos = await repoRes.json();
 
 		res.status(200).json({ userProfile, repos });
