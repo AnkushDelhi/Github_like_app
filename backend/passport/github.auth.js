5084924ce670fa0fc28e4f872d6a48237f50f781
AnkushDelhi <huddaankush8@gmail.com>
2024-04-19 11:03:15 +0530

    railway

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -15,7 +15,7 @@ passport.deserializeUser(function(obj, done) {
  passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
- callbackURL: "https://github-like-app-0p7j.onrender.com/api/auth/github/callback"
+ callbackURL: "https://githublikeapp-production.up.railway.app/api/auth/github/callback"
  },
  		async function (accessToken, refreshToken, profile, done){
       const user = await User.findOne({ username: profile.username });
